1d42b77f4f21b11519e256a1ff008ef9cb550483
loseblue <loseblue@163.com>
2013-02-03 12:43:43 +0800

    no big deal

--- soft/create_tags.bat
+++ soft/create_tags.bat
@@ -1,11 +1,11 @@
-del cscope.out cscope.files tags
-echo del "cscope.out cscope.files tags" successful!
+REM  del cscope.out cscope.files
+REM  echo del "cscope.out cscope.files tags" successful!
 
 REM  echo "ctags create"
 REM  ctags --languages=c --langmap=c:+.h --extra=+q -R
 
 echo "cscope create"
-vim_find . -name "*.h" -o -name "*.c" -o -name "*.cc" > cscope.files
+vim_find . -name "*.h" -o -name "*.c" > cscope.files
 cscope -bCkR -i cscope.files
 
 @echo off
