797b59c53b7f45c535aa0ff117581632a32c64c7
loseblue <loseblue@163.com>
2013-09-25 15:02:40 +0800

    Update copy_tools.bat

--- soft/system/copy_tools.bat
+++ soft/system/copy_tools.bat
@@ -7,5 +7,10 @@ Copy /y cscope_sort.exe %windir%\system32\
 
 Copy /y curl.cmd %ProgramFiles%\Git\cmd\
 xcopy glo628wb C:\glo628wb\ /e/s/d 
+Copy /y ag\ag.exe %windir%\system32\
+Copy /y ag\libgcc_s_dw2-1.dll %windir%\system32\
+Copy /y ag\liblzma-5.dll %windir%\system32\
+Copy /y ag\libpcre-1.dll %windir%\system32\
+Copy /y ag\libz-1.dll %windir%\system32\
 
 pause
